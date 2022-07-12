83c2c0f999d653f51deb4917d3026192ebda166d
Menightmar <jrma.menightmar@gmail.com>
2022-07-12 11:48:50 -0500

    Opciones
    Archivos.

---
+++ include/Funciones_Numericas.h
@@ -0,0 +1,23 @@
+
+
+// -----------------------------------------------------------------------------
+
+// Incluimos los numericos.
+
+#include <Numericos.h>
+
+// -----------------------------------------------------------------------------
+
+// Aqui establecemos el elemento para la utilizacion de los numericos.
+
+class Sumador {
+	
+	// Tenemos como miembro estatico esto :
+	
+	public :
+		
+		// Suma de dos numericos.
+		
+		static Numericos Suma (const Numericos & Valores_A , const Numericos & Valores_B);
+	
+};
---
+++ source/Funciones_Numericas.cpp
@@ -0,0 +1,30 @@
+
+// Definiremos los metodos estaticos de las operaciones con numericos.
+
+#include "Funciones_Numericas.h"
+
+// -----------------------------------------------------------------------------
+
+// Implementamos el sumador.
+
+Numericos Sumador::Suma (const Numericos & Valores_A , const Numericos & Valores_B) {
+	
+	// Creamos un nuevo númerico.
+	
+	Numericos Resultado (0.0 , 0.0);
+	
+	// Asignaremos la suma en cada valor.
+	
+	Resultado.Set_Num_1 (Valores_A.Get_Num_1 () + Valores_B.Get_Num_1 ());
+	
+	// Asignaremos la suma en cada valor.
+	
+	Resultado.Set_Num_2 (Valores_A.Get_Num_2 () + Valores_B.Get_Num_2 ());
+	
+	// Devolvemos el resultado.
+	
+	return Resultado;
+	
+}
+
+// -----------------------------------------------------------------------------
